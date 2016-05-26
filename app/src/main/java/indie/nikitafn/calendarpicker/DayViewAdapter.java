3aea3b5c7494525e5e58db8db25183bdb4ed7cfb
Ncit <nikita@NcitPro.local>
2016-05-26 15:05:47 +0300

    1

---
+++ app/src/main/java/indie/nikitafn/calendarpicker/DayViewAdapter.java
@@ -0,0 +1,6 @@
+package indie.nikitafn.calendarpicker;
+
+/** Adapter used to provide a layout for {@link CalendarCellView}.*/
+public interface DayViewAdapter {
+  void makeCellView(CalendarCellView parent);
+}
